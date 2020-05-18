d0278348b8db1badc12d24577da306f06397854c
GeeksEmpireNet <Support@GeeksEmpire.net>
2020-05-18 09:37:08 -0800

    [Time] 18-May-2020 | 09:35
    [Description]
    
    [Phone][Library][ChangeLog]
    
    [Phone][Sample][ChangeLog]
    
    [Watch][Library][ChangeLog]
    1. Initialized
    
    [Build][ChangeLog]
    # Libraries Updated

--- IndexedFastScrollerLibraryWatch/src/main/java/net/geeksempire/indexedfastscroller/library/Factory/UtilsWatch.kt
+++ IndexedFastScrollerLibraryWatch/src/main/java/net/geeksempire/indexedfastscroller/library/Factory/UtilsWatch.kt
@@ -2,7 +2,7 @@
  * Copyright © 2020 By Geeks Empire.
  *
  * Created by Elias Fazel
- * Last modified 5/18/20 9:12 AM
+ * Last modified 5/18/20 9:36 AM
  *
  * Licensed Under MIT License.
  * https://opensource.org/licenses/MIT
